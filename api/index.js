f7d8644b0e326921dfdd1a884baefb8606912af5
Bhavyasree madineni <bhavyamadineni29@gmail.com>
2024-01-15 00:16:06 +0530

    changes commited

--- api/index.js
+++ api/index.js
@@ -12,14 +12,16 @@ app.get('/api/test', (req,res)=>{
 });
 app.post('/api/transaction',async (req,res)=>{
     // console.log(process.env.MONGO_URL);
-   await mongoose.connect(process.env.MONGO_URL);
+   await mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,});
     const {name,desc,datetime,price} = req.body;
     const transaction = await Transaction.create({name,desc,datetime,price});
     res.json(transaction);
 });
 
 app.get('/api/transactions',async(req,res)=>{
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL,{
+        useNewUrlParser: true,});
     const transactions = await Transaction.find();
     res.json(transactions);
 })
